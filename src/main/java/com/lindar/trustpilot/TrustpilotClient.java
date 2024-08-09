2872f6f1bffa741196fb46e5eae471c9381733d9
Matthias <matthias.portelli@mrq.com>
2024-08-09 16:51:56 +0200

    Adding configurable Trust Pilot base url

--- src/main/java/com/lindar/trustpilot/TrustpilotClient.java
+++ src/main/java/com/lindar/trustpilot/TrustpilotClient.java
@@ -5,9 +5,13 @@ import com.lindar.trustpilot.api.BusinessUnitsPublicResource;
 import com.lindar.trustpilot.api.InvitationResource;
 import com.lindar.trustpilot.api.ServiceReviewResource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class TrustpilotClient {
+
+    private static final String DEFAULT_URL = "https://api.trustpilot.com/v1/";
+    private static final String INVITATIONS_DEFAULT_URL = "https://invitations-api.trustpilot.com/v1/";
 
     private final AuthenticationResource      authenticationResource;
     private final BusinessUnitsPublicResource businessUnitsPublicResource;
@@ -15,10 +19,15 @@ public class TrustpilotClient {
     private final ServiceReviewResource       serviceReviewResource;
 
     public TrustpilotClient() {
-        authenticationResource = new AuthenticationResource();
-        businessUnitsPublicResource = new BusinessUnitsPublicResource();
-        invitationResource = new InvitationResource();
-        serviceReviewResource = new ServiceReviewResource();
+        this(null, null);
+    }
+    public TrustpilotClient(String baseUrl, String invitationsBaseUrl) {
+        baseUrl = StringUtils.isEmpty(baseUrl) ? DEFAULT_URL : baseUrl;
+        invitationsBaseUrl = StringUtils.isEmpty(invitationsBaseUrl) ? INVITATIONS_DEFAULT_URL : invitationsBaseUrl;
+        authenticationResource = new AuthenticationResource(baseUrl);
+        businessUnitsPublicResource = new BusinessUnitsPublicResource(baseUrl);
+        invitationResource = new InvitationResource(invitationsBaseUrl);
+        serviceReviewResource = new ServiceReviewResource(baseUrl);
     }
 
     public AuthenticationResource authentication() {
